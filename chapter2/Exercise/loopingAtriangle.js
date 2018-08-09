0e79f08582409551e490c4208e11578105bcd9de
Bashorun Olajide John <bashorunolajide@gmail.com>
2018-08-09 12:37:23 +0100

    CHAPTER 2: Exercise 1 - LOOPING A TRIAGLE

---
+++ chapter2/Exercise/loopingAtriangle.js
@@ -0,0 +1,17 @@
+/* LOOPING A TRIAGLE
+ * Write a loop that makes seven calls to console.log to output the following triangle:
+ * #
+ * ##
+ * ###
+ * ####
+ * #####
+ * ######
+ * #######
+*/
+
+
+for ( let hash = ""; hash.length <= 7; ){
+
+  console.log (hash +="#");
+}
+
