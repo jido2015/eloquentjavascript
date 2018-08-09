3e7313b955eeab468ad6c340f1fec7ef2eb14469
Bashorun Olajide John <bashorunolajide@gmail.com>
2018-08-09 12:42:43 +0100

    CHAPTER3: EXERCISE 1 - MINIMUM

---
+++ chapter3/Exercise/functionMin.js
@@ -0,0 +1,13 @@
+
+/**MINIMUM
+ * Write a function min that takes two arguments and returns their minimum.
+*/
+
+let min = function (params1, params2) {
+    if(params1> params2){
+        return params2;
+    }else{
+        return params1
+    }
+  }
+  console.log(min(100, 5));
