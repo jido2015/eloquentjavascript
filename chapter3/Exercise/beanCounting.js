5fbafaf0b28063ba2745d2d8f1b2cb25c25d7653
Bashorun Olajide John <bashorunolajide@gmail.com>
2018-08-19 15:38:33 +0100

    CHAPTER 3: EXCERISE 3: Bean counting. add information

--- chapter3/Exercise/beanCounting.js
+++ chapter3/Exercise/beanCounting.js
@@ -1,23 +1,24 @@
+/**BEAN COUNTING 
+ * A function to count capital letter in a given string
+*/
 
-/**BEAN COUNTING */
+function countChar(str, chr) {
+  var counter = 0
+  for (var i = 0; i < str.length; i++) {
 
-  function countChar(str, chr) {
-    var counter = 0
-    for (var i = 0; i < str.length; i++) {
-
-        //If capital letter found, increment counter, 
-        //and keep checking until loop ends(no more letter to check)
-      if (str.charAt(i) === chr) {
-          counter++;
-        }
-    }
-    return counter;
+      //If capital letter found, increment counter, 
+      //and keep checking until loop ends(no more letter to check)
+    if (str.charAt(i) === chr) {
+        counter++;
+      }
   }
+  return counter;
+}
 
-  //Function that uses function counthar(args1, arg2).
-  function countBs(str) {
-    return countChar(str, 'B');
-  }
+//Function that uses function counthar(args1, arg2).
+function countBs(str) {
+  return countChar(str, 'B');
+}
 
 
 console.log(countBs("BannaBoy"));
